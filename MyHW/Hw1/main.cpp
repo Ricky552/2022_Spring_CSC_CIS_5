83f7001f2238c29fcc000242aec441958e74ea20
Ricky552 <lunaricardo318552@gmail.com>
2022-02-26 20:30:03 -0800

    adding hw problem 1

---
+++ MyHW/Hw1/main.cpp
@@ -0,0 +1,24 @@
+ 
+ /* File:   main.cpp
+ * Author: ricky
+ *
+ * Created on February 26, 2022, 8:13 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 50;
+    int num2 = 100;
+    
+    int total = num1 + num2;
+    
+    cout << total;
+    
+    return 0;
+ 
+}
+
+
+
