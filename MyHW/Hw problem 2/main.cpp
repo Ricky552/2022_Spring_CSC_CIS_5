ef5bd892674c633fc600f53d9ab5aec4fa0796e6
Ricky552 <lunaricardo318552@gmail.com>
2022-02-26 21:17:21 -0800

    fixed hw 2 using floats instead of doubles

--- MyHW/Hw problem 2/main.cpp
+++ MyHW/Hw problem 2/main.cpp
@@ -1,6 +1,6 @@
 /* 
  * File:   main.cpp
- * Author: ricky
+ * Author: Ricardo Luna
  *
  * Created on February 26, 2022, 8:31 PM
  */
@@ -11,10 +11,10 @@ using namespace std;
 
 int main() { 
     // storing integers total sales and revenue 
-    double tsales = .58;
-    double revenue = 8600000;
+    float tsales = .58;
+    float revenue = 8600000;
     
-    int total = revenue / tsales;
+    float total = revenue / tsales;
     
     cout << total;
     
---
+++ MyHW/hw 3/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Ricardo Luna
+ *
+ * Created on February 26, 2022, 8:59 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double tsales = 95;
+    double state = .04;
+    double county = .02;
+    
+    double total = tsales / state;
+    
+    cout << total;
+    
+    
+    return 0;
+    }
+
