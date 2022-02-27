47e08974e886b9aa1636cbeeb4805565f03480fc
Ricky552 <lunaricardo318552@gmail.com>
2022-02-26 20:57:30 -0800

    added hw problem 2

---
+++ MyHW/Hw problem 2/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: ricky
+ *
+ * Created on February 26, 2022, 8:31 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() { 
+    // storing integers total sales and revenue 
+    double tsales = .58;
+    double revenue = 8600000;
+    
+    int total = revenue / tsales;
+    
+    cout << total;
+    
+    return 0;
+}
--- MyHW/Hw1/main.cpp
+++ MyHW/Hw1/main.cpp
@@ -1,7 +1,7 @@
  
  /* File:   main.cpp
- * Author: ricky
- *
+ * Author: Ricardo Luna
+ *Programming challenges number 1 
  * Created on February 26, 2022, 8:13 PM
  */
 
@@ -9,9 +9,11 @@
 using namespace std;
 
 int main() {
+    // store integers 100 and 50 in variables
     int num1 = 50;
     int num2 = 100;
     
+    //sum of these two variables 
     int total = num1 + num2;
     
     cout << total;
